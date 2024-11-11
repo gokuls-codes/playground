2fd098a5fcc30bca376817f8ee3ed4eec4b2ce50
Gokul <gokulgk9402@gmail.com>
2024-11-11 08:51:16 +0530

    added text sliding up animation on hover

--- src/app/animations/page.tsx
+++ src/app/animations/page.tsx
@@ -25,6 +25,16 @@ const AnimationsPage = () => {
               <div className=" h-10 w-1 bg-white rounded-sm absolute group-hover:rotate-45 transition-all duration-100"></div>
             </div>
           </div>
+          <div className=" h-[120px] col-span-2 w-full border border-gray-700 rounded-md flex items-center justify-center">
+            <div className="  relative overflow-hidden group text-xl font-semibold">
+              <p className=" group-hover:-translate-y-[100%] duration-200 transition-all">
+                Label
+              </p>
+              <p className=" absolute group-hover:-translate-y-[100%] duration-200 transition-all ">
+                Label
+              </p>
+            </div>
+          </div>
           <div className=" w-full col-span-3 lg:col-span-6 flex items-center relative group border border-gray-700 rounded-md p-4 h-[120px]">
             <div className=" w-full h-[1px] bg-white/50 rounded-sm"></div>
             <div className=" w-[calc(100%-2rem)] h-1 bg-white rounded-sm absolute scale-x-0 group-hover:scale-x-100 transition-all duration-300 origin-left"></div>
