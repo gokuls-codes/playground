0fb23435ae1e474b5bf0aebd3860a523fd0b08ad
Gokul <gokulgk9402@gmail.com>
2025-02-24 16:26:17 +0530

    test streaming affecting seo

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,13 +1,15 @@
 import ClientComponent from "@/components/test/client-component";
 import ServerComponent from "@/components/test/server-component";
-import React from "react";
+import React, { Suspense } from "react";
 
 const TestPage = async () => {
   return (
     <div className=" mx-auto w-full max-w-md">
       <h1>Test Page</h1>
       <ClientComponent />
-      <ServerComponent />
+      <Suspense fallback={<div>loading...</div>}>
+        <ServerComponent />
+      </Suspense>
     </div>
   );
 };
