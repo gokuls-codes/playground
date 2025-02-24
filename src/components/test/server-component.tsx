9347df79c5b9f2d6f4fee72b34444392ace831cd
Gokul <gokulgk9402@gmail.com>
2025-02-24 16:30:55 +0530

    test server component with api call

--- src/components/test/server-component.tsx
+++ src/components/test/server-component.tsx
@@ -3,7 +3,15 @@ import React from "react";
 const ServerComponent = async () => {
   await new Promise((r) => setTimeout(r, 2000));
 
-  return <div>server component that is streamed</div>;
+  const resp = await fetch("https://jsonplaceholder.typicode.com/todos/", {
+    cache: "no-store",
+  });
+
+  const respJson = await resp.json();
+
+  return (
+    <div>server component that is streamed with {respJson?.length} todos</div>
+  );
 };
 
 export default ServerComponent;
