8afc057ca12ae6071f84da9951a9be5066474f3e
Gokul <gokulgk9402@gmail.com>
2025-02-27 19:11:09 +0530

    added delay in GET request route handler

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,5 @@
-export function GET() {
+export async function GET() {
+  const temp = await new Promise((r) => setTimeout(r, 2000));
+  console.log(temp);
   return Response.json({ message: "Hello from Next.js!", time: new Date() });
 }
