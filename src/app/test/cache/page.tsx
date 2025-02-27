7d47f7ce7a281f852d4533b64117c7e8220fa572
Gokul <gokulgk9402@gmail.com>
2025-02-27 19:14:40 +0530

    minor change

--- src/app/test/cache/page.tsx
+++ src/app/test/cache/page.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 const TestCachePage = async () => {
-  const resp = await fetch("http://localhost:3000/api/hello", {
-    next: {
-      revalidate: 10,
-    },
-  });
-  const data = await resp.json();
+  //   const resp = await fetch("http://localhost:3000/api/hello", {
+  //     next: {
+  //       revalidate: 10,
+  //     },
+  //   });
+  //   const data = await resp.json();
 
-  console.log(data);
+  //   console.log(data);
 
-  return <div>{data.time}</div>;
+  //   return <div>{data.time}</div>;
+
+  return <div></div>;
 };
 
 export default TestCachePage;
