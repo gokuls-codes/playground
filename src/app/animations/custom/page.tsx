bfc8d8edb515301cb902ed7d7bee86c61c932b34
Gokul <gokulgk9402@gmail.com>
2025-04-29 17:58:52 +0530

    added a component with a glow going across its radius

---
+++ src/app/animations/custom/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const CustomAnimationsPage = () => {
+  return (
+    <div className="  p-8">
+      <main className=" w-full lg:max-w-[70vw] mx-auto space-y-6">
+        <h1 className=" text-4xl font-semibold">Animations</h1>
+        <section className=" grid place-items-center">
+          <div className=" card bg-neutral-900 grid place-items-center relative rounded-xl">
+            <p className=" text-4xl font-semibold  ">Hello</p>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default CustomAnimationsPage;
