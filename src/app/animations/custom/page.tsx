fbcff5958989f67bf82d40f74a10b1fa128adb1a
Gokul <gokulgk9402@gmail.com>
2025-04-29 18:54:24 +0530

    minor fix in glow going around border

--- src/app/animations/custom/page.tsx
+++ src/app/animations/custom/page.tsx
@@ -6,8 +6,10 @@ const CustomAnimationsPage = () => {
       <main className=" w-full lg:max-w-[70vw] mx-auto space-y-6">
         <h1 className=" text-4xl font-semibold">Animations</h1>
         <section className=" grid place-items-center">
-          <div className=" card bg-neutral-900 grid place-items-center relative rounded-xl">
-            <p className=" text-4xl font-semibold  ">Hello</p>
+          <div className=" card  grid place-items-center relative rounded-xl p-0.5">
+            <div className="bg-neutral-900 h-full w-full rounded-xl grid place-items-center">
+              <p className=" text-4xl font-semibold  ">Hello</p>
+            </div>
           </div>
         </section>
       </main>
