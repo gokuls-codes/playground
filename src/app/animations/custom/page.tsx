df42f7057cfb1ac050a6b9d5ca8bde3f397f9a73
Gokul <gokulgk9402@gmail.com>
2025-04-30 12:36:02 +0530

    minor changes

--- src/app/animations/custom/page.tsx
+++ src/app/animations/custom/page.tsx
@@ -6,8 +6,8 @@ const CustomAnimationsPage = () => {
       <main className=" w-full lg:max-w-[70vw] mx-auto space-y-6">
         <h1 className=" text-4xl font-semibold">Animations</h1>
         <section className=" grid place-items-center">
-          <div className=" card  grid place-items-center relative rounded-xl p-0.5">
-            <div className="bg-neutral-900 h-full w-full rounded-xl grid place-items-center">
+          <div className=" card  grid place-items-center relative rounded-xl p-0.5 bg-neutral-800">
+            <div className="bg-neutral-900 h-full w-full rounded-xl grid place-items-center z-10">
               <p className=" text-4xl font-semibold  ">Hello</p>
             </div>
           </div>
