a7d292159a1ccead70203c4f6b5436740578fab5
Gokul <gokulgk9402@gmail.com>
2025-06-12 16:06:29 +0530

    added zoom-test page

---
+++ src/app/zoom/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ZoomPage = () => {
+  return (
+    <div className=" grid place-items-center h-screen w-screen">
+      <div className=" size-40 bg-white border-2 border-slate-400 rounded-md zoom-div sm:bg-indigo-400 md:bg-blue-400 lg:bg-green-400 xl:bg-yellow-400 2xl:bg-orange-400 "></div>
+    </div>
+  );
+};
+
+export default ZoomPage;
