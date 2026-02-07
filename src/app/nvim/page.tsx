3f02feb4a52220f78759e64a71bbb8e9971b54c8
Gokul Kannan <gokulscodes@gmail.com>
2026-02-07 14:40:31 +0530

    udpate treesitter

---
+++ src/app/nvim/page.tsx
@@ -0,0 +1,22 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>nvim</h1>
+      <p>nvim is a text editor that is built to be used in the terminal.</p>
+      <a href="https://neovim.io/" target="_blank">
+        neovim.io
+      </a>
+
+      <h2>It is a good editor</h2>
+      <div>
+        <p>test paragraph</p>
+
+        <span>test span</span>
+
+        <li>test li</li>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
